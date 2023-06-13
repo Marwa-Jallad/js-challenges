aa756073928cb0fe2179bf108e330b05e4d263c4
Marwa-Jallad <marwa.jallad89@gmail.com>
2023-06-13 22:53:38 +0300

    dealing with Json object

--- E169-178.js
+++ E169-178.js
@@ -1,1 +1,16 @@
-console.log("Hello")
+console.log("Hello");
+
+const jasonElement  = '{"userNmae": "Marwa"}'
+console.log ( typeof jasonElement);
+console.log (jasonElement);
+
+const jsElement = JSON.parse(jasonElement);
+console.log ( typeof jsElement);
+console.log (jsElement)
+
+jsElement["userNmae"] = "Marwa" ;
+// jsElement["age"] = 34;
+
+const jasonElementToServer = JSON.stringify (jsElement)
+console.log ( typeof jasonElementToServer);
+console.log (jasonElementToServer)
