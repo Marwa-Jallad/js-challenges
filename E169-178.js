fc6d182305f7fa733f732e0aa45e18c14e70a0ef
Marwa-Jallad <marwa.jallad89@gmail.com>
2023-06-23 16:42:17 +0300

    callback hell

--- E169-178.js
+++ E169-178.js
@@ -33,11 +33,43 @@ let myRequest = new XMLHttpRequest();
 myRequest.open ("GET","http://api.github.com/users/elzerowebschool/repos");
 myRequest.send()
 console.log(myRequest)
-
 myRequest.onreadystatechange = function (){
     console.log(myRequest.readyState);
     console.log(myRequest.status);
     if (this.readyState === 4 && this.status === 200){
-        console.log(this.responseText)
+        let jsData = JSON.parse(this.responseText);
+        // console.log(jsData);
+        for (i=0; i<jsData.length;i++){
+            let div = document.createElement("div");
+            let repoName =document.createTextNode(jsData[i].full_name);
+            div.appendChild(repoName);
+            document.body.appendChild(div)
+        }
     }
 }
+
+function makeItRed(e) {
+    e.target.style.color = "red";
+}
+
+let p = document.getElementsByClassName("test")[0];
+p.addEventListener("click" , makeItRed); 
+
+function iamAcallBack (){
+    console.log("i am a Call back Function")
+}
+
+setTimeout(iamAcallBack,2000);
+
+setTimeout(() => {
+    console.log("download photo from url");
+    setTimeout(() => {
+        console.log("resize photo");
+        setTimeout(() => {
+            console.log("add logo to the photo") 
+            setTimeout(() => {
+                console.log("show photo on the website")   
+            }, 4000);              
+        }, 3000);
+    }, 2000);
+}, 1000);
